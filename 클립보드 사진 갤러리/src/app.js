996866f4b492f50ca55a8150f87e55a016930b10
eanby00 <wjdwnstjd00@gmail.com>
2024-07-09 15:53:05 +0900

    clipboard의 사진 불러오기

--- 클립보드 사진 갤러리/src/app.js
+++ 클립보드 사진 갤러리/src/app.js
@@ -1,7 +1,21 @@
 const TYPE_IMAGE = "image/png";
 const image = document.querySelector("img");
 
+class Image {}
+
+class ImageList {
+  imageList = [];
+
+  constructor() {}
+
+  push(imageSrc) {
+    this.imageList.push(imageSrc);
+  }
+}
+
 class ClipboardHelper {
+  lastBlob;
+
   static async readImage() {
     const clipboardData = await navigator.clipboard.read();
     const data = clipboardData[0];
@@ -9,16 +23,22 @@ class ClipboardHelper {
       return;
     }
     const blobData = await data.getType(TYPE_IMAGE);
+
+    if (blobData === this.lastBlob) {
+      return;
+    }
+    this.lastBlob = blobData;
     return window.URL.createObjectURL(blobData);
   }
 }
 
 class App {
   static init() {
-    // const timer = setInterval(() => {
-    //   console.log("test");
-    // }, 10000);
-    console.log(ClipboardHelper.readImage());
+    const imageList = new ImageList();
+    const timer = setInterval(() => {
+      imageList.push(ClipboardHelper.readImage());
+      console.log(imageList.imageList);
+    }, 10000);
   }
 }
 
